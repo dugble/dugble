61c5d4fbaeac865d6cdcdf20457ea35b62a3042c
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-14 09:20:15 +0000

    refactor(worker): use registry composition root

--- server/cmd/worker/main.go
+++ server/cmd/worker/main.go
@@ -5,11 +5,11 @@ import (
 
 	sentrymonitoring "github.com/dugble/dugble/server/internal/adapters/monitoring/sentry"
 
-	dugbleworker "github.com/dugble/dugble/server/internal/dugble/worker"
+	workerregistry "github.com/dugble/dugble/server/internal/registry/worker"
 )
 
 func main() {
-	if err := dugbleworker.Start(); err != nil {
+	if err := workerregistry.Start(); err != nil {
 		sentrymonitoring.Error("worker stopped", "error", err)
 		os.Exit(1)
 	}
