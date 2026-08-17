3e2f16019a5c549aac13a95b13ed0809abb65339
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-08-17 13:23:38 +0000

    delegate sender ID validation to relay

--- server/internal/platform/senderid/validation.go
+++ server/internal/platform/senderid/validation.go
@@ -1,37 +1,19 @@
 package senderid
 
 import (
-	"fmt"
-	"regexp"
 	"strings"
-	"unicode/utf8"
+
+	relaysenderid "github.com/dugble/dugble/server/internal/relay/senderid"
 )
 
-const MaxNameCharacters = 11
-
-var (
-	nameCharactersPattern = regexp.MustCompile(`^[A-Za-z0-9]+$`)
-	nameHasLetterPattern  = regexp.MustCompile(`[A-Za-z]`)
-)
+const MaxNameCharacters = relaysenderid.MaxNameCharacters
 
 func NormalizeName(name string) string {
-	return strings.TrimSpace(name)
+	return relaysenderid.NormalizeName(name)
 }
 
 func ValidateName(name string) error {
-	name = NormalizeName(name)
-	switch {
-	case name == "":
-		return fmt.Errorf("sender ID name is required")
-	case utf8.RuneCountInString(name) > MaxNameCharacters:
-		return fmt.Errorf("sender ID name must be at most %d characters", MaxNameCharacters)
-	case !nameCharactersPattern.MatchString(name):
-		return fmt.Errorf("sender ID name may contain only letters and numbers")
-	case !nameHasLetterPattern.MatchString(name):
-		return fmt.Errorf("sender ID name must contain at least one letter")
-	default:
-		return nil
-	}
+	return relaysenderid.ValidateName(name)
 }
 
 func (request CreateRequest) Normalize() CreateRequest {
